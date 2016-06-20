bba894cfc02f9c65f3b0a4590ac7251d7a439a3b
AudioGod <audiogod.adi@gmail.com>
2016-06-20 02:18:18 +0000

    Sepolicy: Squashed Update
    Signed-off-by: AudioGod <audiogod.adi@gmail.com>

--- Android.mk
+++ Android.mk
@@ -77,6 +77,7 @@ $(sepolicy_policy.conf) : $(call build_policy, $(sepolicy_build_files))
 	@mkdir -p $(dir $@)
 	$(hide) m4 -D mls_num_sens=$(PRIVATE_MLS_SENS) -D mls_num_cats=$(PRIVATE_MLS_CATS) \
 		-D target_build_variant=$(TARGET_BUILD_VARIANT) \
+		-D shipping_build=$(YU_TARGET) \
 		-s $^ > $@
 	$(hide) sed '/dontaudit/d' $@ > $@.dontaudit
 
